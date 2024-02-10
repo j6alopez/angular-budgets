fdca9388c54d3bde393eee52c64a56d1d49e6fa6
jose <j6alopez@gmail.com>
2024-02-10 02:34:22 +0100

    feat: add spain as default locale in i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [ 
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ]
 };
