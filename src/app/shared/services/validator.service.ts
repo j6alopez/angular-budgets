d1604ce44cfb1206f8121f887f3701c2c45c1230
jose <j6alopez@gmail.com>
2024-03-04 16:46:09 +0100

    fix: validation services pattern allows minimum 2 words

--- src/app/shared/services/validator.service.ts
+++ src/app/shared/services/validator.service.ts
@@ -6,7 +6,7 @@ import { AbstractControl, FormArray, FormGroup, ValidationErrors, ValidatorFn } 
 })
 export class ValidatorService {
 
-  public static firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public static firstNameAndLastnamePattern: string = '^[a-zA-Z]+ [a-zA-Z]+(?: [a-zA-Z]+)*$';
   public static emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
   public static spanishPhonePattern: string = '^[679][0-9]{8}$';
   public static onlyNumbersPattern: string = '^[1-9]\d*$';
